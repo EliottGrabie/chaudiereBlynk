18f54803a2de31d12f8fac2e6166e4f529386037
Grabie Eliott <eliott.grabie@gmail.com>
2018-12-18 22:03:11 +0100

    cool

--- chaudiereBlynk.ino
+++ chaudiereBlynk.ino
@@ -52,8 +52,6 @@ https://github.com/PaulStoffregen/Time/blob/master/examples/TimeSerial/TimeSeria
 #include <BlynkSimpleCC3000.h>
 #include <TimeLib.h>
 
-
-
 // Your WiFi credentials.
 // Choose wifi_sec from WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 char ssid[] = "Livebox-f1fc";
@@ -69,88 +67,9 @@ bool etatChauffeau = false;
 
 BlynkTimer timer;
 
-int jour = 1;
-bool semaine[7][12] = {
-	//	0	1		2		3		4	5		6		7		8	9		10		11		
-	{ true, false ,false, false, false ,false, false, false ,false, false, false ,false },	//Lundi
-	{ false, true ,false, false, false ,false, false, false ,false, false, false ,true },	//Mardi
-	{ false, false, true, false, false, false, false, false, false, false, true, false },	//Mercredi
-	{ false, false, false, true, false, false, false, false, false, true, false, false },	//Jeudi
-	{ false, false, false, false, true, false, false, false, true, false, false, false },	//Vendredi
-	{ false, false, false, false, false, true, false, true, false, false, false, false },	//Samedi
-	{ false, false, false, false, false, false, true, false, false, false, false, false },	//Dimanche
-};
-
-WidgetTable table;
-BLYNK_ATTACH_WIDGET(table, V11);
-
 BLYNK_CONNECTED() {
 	//get data stored in virtual pin V0 from server
-	Blynk.syncVirtual(V0);
-}
-/*
-BLYNK_WRITE(V0)
-{
-	//restoring value
-	//semaine[][] = param.asInt();
-	//Serial.print("syncVirtual : ");
-	//Serial.println(param.asString());
-	
-	String retSemaine = param.asString();
-	int iCar = 0;
-	for (int col = 0; col < 12; col++) {
-
-		//Serial.print("col : ");
-		//Serial.println(col);
-
-		char str[5] = "0x";
-		
-		str[2] = retSemaine[iCar+col];
-		iCar++;
-		str[3] = retSemaine[iCar+col];
-		//iCar++;
-				
-		int num;
-		//Serial.print("str : ");
-		//Serial.println(str);
-
-		sscanf(str, "%x", &num);
-
-		//Serial.print("num : ");
-		//Serial.println(num);
-		unsigned int iToByte = byte(num);
-		
-		//iToByte -= 65;
-		//Serial.print("iToByte : ");
-		//Serial.println(iToByte);
-
-		byte creneau = byte(iToByte);
-		//Serial.print("creneau : ");
-		//Serial.println(creneau,BIN);
-		
-		//Serial.println("creneau: ");
-		for (int _jour = 0; _jour < 7; _jour++) {
-			if (creneau & (1 << (6 - _jour)))
-			{
-				//Serial.print("1");
-				semaine[_jour][col] = true;
-			}
-			else
-			{
-				//Serial.print("0");
-				semaine[_jour][col] = false;
-			}
-		}
-		
-		//Serial.println("");
-		
-		//delay(500);
-	}
-}
-*/
-BLYNK_WRITE(V10) {
-	actuTableau(param.asInt());
-	jour = param.asInt();
+	Blynk.syncAll();
 }
 
 BLYNK_WRITE(V12) {
@@ -169,102 +88,6 @@ BLYNK_WRITE(V12) {
 }
 
 
-String makeHeure(int index) {
-
-	String heure = "";
-	heure = heure + (index * 2);
-	heure = heure + ":00 - ";
-	heure = heure + ((index * 2) + 2);
-	heure = heure + ":00";
-	return heure;
-}
-
-void actuTableau(int _jour) {
-	table.clear(); //Cleart Tableau
-	_jour--;
-	for (size_t i = 0; i < sizeof(semaine[_jour]); i++)	//Parcourt des tranche d'heure
-	{
-		String etat = "OFF";
-
-		if (semaine[_jour][i]) {
-			etat = "ON";
-		}
-		table.addRow(i, makeHeure(i), etat);
-		//table.pickRow(i);
-		if (semaine[_jour][i] == false) {
-			Blynk.virtualWrite(V11, "deselect", i);
-		}
-	}
-
-}
-
-void checkClock()
-{
-
-	String currentTime = String(hour()) + ":" + minute() + ":" + second();
-	String currentDate = String(day()) + " " + month() + " " + year();
-
-	String jSemaine;
-	switch (dayOfWeek(now()))
-	{
-	case 1:
-		jSemaine = "Dimmanche";
-		break;
-	case 2:
-		jSemaine = "Lundi";
-		break;
-	case 3:
-		jSemaine = "Mardi";
-		break;
-	case 4:
-		jSemaine = "Mercredi";
-		break;
-	case 5:
-		jSemaine = "Jeudi";
-		break;
-	case 6:
-		jSemaine = "Vendredi";
-		break;
-	case 7:
-		jSemaine = "Samedi";
-		break;
-	default:
-		break;
-	}
-
-	Blynk.virtualWrite(V1, jSemaine);
-	//Serial.println(dayOfWeek(now()) - 2);
-	//Serial.println(int(hour() / 2));
-	if (semaine[dayOfWeek(now()) - 2][int(hour()/2)])
-	{
-		changeChauffage(true);
-	}
-	else
-	{
-		changeChauffage(false);
-	}
-
-}
-/*
-void printTableau() {
-	for (size_t _jour = 0; _jour < 7; _jour++)
-	{
-		for (size_t crn = 0; crn < 12; crn++)
-		{
-			if (semaine[_jour][crn])
-			{
-				Serial.print("1");
-			}
-			else
-			{
-				Serial.print("0");
-			}
-		}
-		Serial.println("");
-	}
-	Serial.println("");
-}
-*/
 bool changeChauffage(bool etat) {
 	if (etat)
 	{
@@ -309,57 +132,10 @@ void setup()
 		//Serial.println();
 	});
 	*/
-	table.onSelectChange([](int index, bool selected) {
 
-		String msg = "";
-
-		int _jour = jour-1;
-		if (selected) {
-			semaine[_jour][index] = true;
-			table.updateRow(index, makeHeure(index), "ON");
-		}
-		else{
-			semaine[_jour][index] = false;
-			Blynk.virtualWrite(V11, "update", index, makeHeure(index), "OFF");
-		}
-		
-		// send to serveur pour save
-		for (int col = 0; col < 12; col++) {
-			byte bToInt = 0;
-			//Serial.print("creneau : ");
-			for (int _jour2 = 0; _jour2 < 7; _jour2++) {
-				//Serial.print(semaine[_jour2][col]);
-				if (semaine[_jour2][col]) {
-					bToInt |= 1 << (6-_jour2);
-				}
-			}
-			//Serial.println("");
-			//Serial.print("bToInt : ");
-			//Serial.println(bToInt, BIN);
-			char car = char(bToInt);
-			char hexa[3];
-			
-			sprintf(hexa, "%.2X", car);
-			//Serial.println(hexa,BIN);
-			msg += hexa;
-
-		}
-		//Serial.print("msg : ");
-		//Serial.println(msg);
-
-		Blynk.virtualWrite(V0, msg);
-
-	});
-
-	Blynk.virtualWrite(V10, jour); 
-	actuTableau(jour);
-	//delay(1000);
-	// Display digital clock every 300000 = 5 min
-	timer.setInterval(30000L, checkClock);
 }
 
 void loop()
 {
 	Blynk.run();
-	timer.run();
 }
